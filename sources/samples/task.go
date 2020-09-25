2cc801c166574233236809c3449842e61878c3bc
aaaaaaaalesha <sks2311211@mail.ru>
2020-09-25 23:38:39 +0300

    Init

---
+++ sources/samples/task.go
@@ -0,0 +1,12 @@
+package samples
+
+import "time"
+
+type Task struct {
+        Id               int       `json:"id"`
+	Title            string    `json:"title"`             // Название таска
+	Description      string    `json:"description"`       // Описание таска.
+	NotificationFreq int64     `json:"notification_freq"` // Время, которое спят уведомления
+	IsAccomplished   bool      `json:"accomplished"`      // Показывает, выполнена ли таска.
+	CreationTime     time.Time `json:"creation_time"`     // Время создания таска.
+}
