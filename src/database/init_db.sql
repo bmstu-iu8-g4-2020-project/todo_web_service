e6a14adcea806d9f8b68778afae63efb19b76769
aaaaaaaalesha <sks2311211@mail.ru>
2020-10-24 23:39:25 +0300

    added field to init_db.sql

--- src/database/init_db.sql
+++ src/database/init_db.sql
@@ -3,5 +3,6 @@ drop table if exists tg_user cascade;
 CREATE TABLE if not exists task_table
 (
     id       SERIAL PRIMARY KEY UNIQUE,
-    username varchar(255)
+    username varchar(255),
+    user_id  integer
 );
