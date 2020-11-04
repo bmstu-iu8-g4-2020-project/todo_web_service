c25e28208b466d82b328cb87e8d698ebc6c79306
aaaaaaaalesha <sks2311211@mail.ru>
2020-11-04 18:54:30 +0300

    Init models

--- src/database/init_db.sql
+++ src/database/init_db.sql
@@ -1,5 +1,6 @@
 drop table if exists tg_user cascade;
 drop table if exists fast_task cascade;
+drop table if exists schedule cascade;
 
 CREATE TABLE if not exists tg_user (
     user_id  integer unique,
@@ -15,4 +16,20 @@ CREATE TABLE if not exists fast_task (
     task_name varchar(255),
     notify_interval bigint,
     deadline timestamptz
+);
+
+CREATE TABLE if not exists schedule (
+    id serial primary key,
+    assignee_id integer references tg_user(user_id),
+    task_name varchar(10)
+);
+
+CREATE TABLE if not exists schedule_task (
+    id serial primary key,
+    schedule_id integer references schedule(id),
+    title varchar(255),
+    place varchar(50),
+    speaker varchar(50),
+    start_time timestamp,
+    start_end timestamp
 );
---
+++ src/models/schedule.go
@@ -0,0 +1,19 @@
+package models
+
+import "time"
+
+type Schedule struct {
+	Id         int          `json:"id"`
+	AssigneeId int          `json:"assignee_id"`
+	WeekDay    time.Weekday `json:"week_day"`
+}
+
+type ScheduleTask struct {
+	Id         int       `json:"id"`
+	ScheduleId int       `json:"schedule_id"`
+	Title      string    `json:"title"`
+	Place      string    `json:"place"`
+	Speaker    string    `json:"speaker"`
+	Begin      time.Time `json:"begin"`
+	End        time.Time `json:"end"`
+}
